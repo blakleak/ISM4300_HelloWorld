b499fe81655d5e171116d8116671a30e3cd47493
Amanda Blakley <51011635+blakleak@users.noreply.github.com>
2020-01-23 11:10:37 -0500

    Hello World Version 2

--- ISM4300_HelloWorld/Program.cs
+++ ISM4300_HelloWorld/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// Name: Amanda Blakley
+// Date: 1/21/2020
+// Project: Deliverable 1 - "Hello World"
+// Description: Creates a basic console application that displays the message "Hello World!". 
+
+using System;
 
 namespace ISM4300_HelloWorld
 {
@@ -6,7 +11,18 @@ namespace ISM4300_HelloWorld
     {
         static void Main(string[] args)
         {
+            // Pause to prior to displaying "Hello World!"
+            Console.WriteLine("Press any key to continue...");
+
+            // Creates a space 
+            Console.WriteLine("   ");
+
+            // Write a line that states "Hello World!"
             Console.WriteLine("Hello World!");
+
+            // Console is active so user can read the message. 
+            // Press any key to exit the application 
+            Console.ReadKey(true);
         }
     }
 }
